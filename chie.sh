e07010e99ab1c912ebf1a9a1634b31b46a3f324b
kurokoji <shu.kakihana@gmail.com>
2016-12-09 14:10:46 +0900

    first

---
+++ chie.sh
@@ -0,0 +1,14 @@
+`g++ -std=c++11 -O2 -Wall ${1}`; then
+  echo "SUCCESS COMPILE"
+  for i in `seq 1 5`
+  do
+    cd $(dirname $0)
+    P=`pwd`
+    DIR=`basename ${P}`
+    str="2017-yo-t${DIR}-in${i}.txt"
+    ./a.out < $str > "out-${DIR}-${i}.txt"
+    echo "SUCCESS out-${DIR}-${i}.txt"
+  done
+else
+  echo "Failed"
+fi 
